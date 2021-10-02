3e4a2fa72e341927868ea0cd3063bfc37bbad0b1
Marufa-Zmakan <marufa@zmakan.com>
2021-10-02 19:46:58 +0300

    [+]pragmatic code

---
+++ pragtech_geolocation_share/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+{
+  "name"                 :  "Website Customer Geolocation Address",
+  "summary"              :  """Share geolocation on shipping page""",
+  "category"             :  "website",
+  'version'              : '13.0.11',
+  "sequence"             :  1,
+  'license'              : 'AGPL-3',
+  'author'               : 'Pragmatic TechSoft Pvt Ltd.',
+  'website'              : 'http://www.pragtech.co.in',
+  "description"          :  """This module works very well with latest version of Odoo 13.0
+--------------------------------------------------------------""",
+  "depends"              :  ['website_sale'],
+  "data"                 :  [
+                              'views/template_checkout.xml',
+                              'views/website_templates.xml',
+                              'views/res_partner_view.xml',
+                              'data/data_geolocation.xml',
+
+
+
+                             ],
+  "images"               :  ['static/description/Banner.png'],
+  "application"          :  True,
+  "installable"          :  True,
+  "auto_install"         :  False,
+
+}
